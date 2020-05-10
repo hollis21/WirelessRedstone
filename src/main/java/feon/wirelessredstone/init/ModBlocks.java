3b6031192b932ce36cff4a46f46fa6045ede7d24
Mike Hollis <hollis21@hotmail.com>
2020-05-10 15:39:08 -0400

    Clean up extra imports

--- src/main/java/feon/wirelessredstone/init/ModBlocks.java
+++ src/main/java/feon/wirelessredstone/init/ModBlocks.java
@@ -6,13 +6,7 @@ import feon.wirelessredstone.objects.blocks.RedstoneTransmitter;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
